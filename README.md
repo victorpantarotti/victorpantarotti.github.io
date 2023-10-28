b03f4d8f4114a28f27d445c6a400538293f32531
Fvictorpantarotti <victorpantarotti@gmail.com>
2023-10-27 23:13:18 -0300

    Website Upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# victorpantarotti.github.io
+# victorpantarotti's website
+
+**um site simples pra ficar no domínio [victorpantarotti.com.br](https://victorpantarotti.com.br/)  
+feito com html, css e js na repl.it em uma tarde**
+
+### créditos/fontes:  
+topography image: [deskpads.gallery](https://deskpads.gallery/wp-content/uploads/2022/07/Topography-white-on-black-1.png)  
+icons: [fontAwesome](https://fontawesome.com/v5/search)  
+loader: [codepen](https://codepen.io/jkantner/pen/mdKOpbe)  
+vhs glitch effect: [gif](https://i.gifer.com/origin/23/23e9de756d6b33a400142b47c8ba6418.gif)
---
+++ script.js
@@ -0,0 +1,39 @@
+// Loader
+
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {    
+    document.querySelector("content").style.visibility = "visible";
+    document.querySelector("loader").classList.add("loaderOut");
+    document.querySelector("body").style.overflow = "visible";
+  } 
+}
+
+// Title Center Section
+
+const messages = [
+  "to sem ideia, toma uns negocio ae",
+  "nada para ver aqui!",
+  "se vc veio procurar algo, procurou no lugar errado :)",
+  ">:)",
+  "umas coisinhas ae"
+];
+
+const centerTitle = document.querySelector("#center #title");
+centerTitle.innerText = messages[Math.floor(Math.random() * messages.length)];
+
+// Songs
+
+const songs = [
+  "0eOKg88Jcew", // for my family
+  "6GMKYueN4mM", // by the way
+  "Ikweq0THs88", // oh, pretty woman
+  "qU9mHegkTc4", // 505
+  "mV01vajK3C4", // dreamin'
+  "o2PoHJfknUI", // i'm gone
+  "-ufLmFKGTwA", // solstício
+  "nTQE44GVzT4", // i just don't know what to do with myself
+];
+
+const iframe = document.querySelector('iframe');
+const song = songs[Math.floor(Math.random() * songs.length)];
+iframe.src = `https://www.youtube.com/embed/${song}`;
